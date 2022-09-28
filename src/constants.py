71696bb4c3ed9c1a045daaf26adc28b39f6f909a
Андрей Изотов <andrej.izotoff@yandex.ru>
2022-09-28 18:46:25 +0300

    первый коммит

---
+++ src/constants.py
@@ -0,0 +1,18 @@
+import re
+from pathlib import Path
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+PEP_URL = 'https://peps.python.org/'
+BASE_DIR = Path(__file__).parent
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+PATTERN = re.compile("^Status$")
+EXPECTED_STATUS = {
+    'A': ['Active', 'Accepted'],
+    'D': ['Deferred'],
+    'F': ['Final'],
+    'P': ['Provisional'],
+    'R': ['Rejected'],
+    'S': ['Superseded'],
+    'W': ['Withdrawn'],
+    '': ['Draft', 'Active'],
+}
